be17d2c0d4beed2b20d2230de0db5673741a2257
gabitza-tech <gabriel.pirlogeanu@gmail.com>
2024-04-05 17:13:06 +0200

    sped up enrollment sampling by 10x

--- cvn_sr/src/few_shot_identification.py
+++ cvn_sr/src/few_shot_identification.py
@@ -27,8 +27,8 @@ import logging
 from nemo.collections.asr.parts.utils.speaker_utils import embedding_normalize
 from scripts.utils import majority_element
 # Set the seed to a fixed value (for reproducibility)
-
-
+import time
+from utils.utils import sampler_query,sampler_support, simpleshot, compute_acc
 
 def setup_logger(log_file):
     # Create a logger
@@ -62,6 +62,7 @@ def main(cfg):
 
     logging.info(f'Hydra config: {OmegaConf.to_yaml(cfg)}')
 
+    # Create logger file
     if cfg.data.out_file is not None:
         log_name = cfg.data.out_file+"_"+str(cfg.n_way)+"_"+str(cfg.k_shot)+"_"+str(cfg.n_tasks)
     else:
@@ -69,6 +70,7 @@ def main(cfg):
 
     logger = setup_logger(f"{log_name}.log")
 
+    # Load embeddings and labels dictionaries for enrollment and test
     enroll_dict = load_pickle(cfg.data.enrollment_embs)
     test_dict = load_pickle(cfg.data.test_embs)
 
@@ -88,151 +90,41 @@ def main(cfg):
 
     task_accs = []
     for i in range(cfg.n_tasks):
+        
         # Randomly select the classes to be analyzed and the files from each class
         # For each task, we use a certain seed
+        task_start_time = time.time()
         task_seed = random_numbers[i]
+        np.random.seed(task_seed)
         random.seed(task_seed)
 
         # We sample cfg.n_way classes out of the total number of classes
         sampled_classes = sorted(random.sample(uniq_classes, cfg.n_way))
-        #logger.info(f"For task {i}, number of classes are {len(sampled_classes)} and selected classes are: {sampled_classes}")
-        
-        # We find which indices in the lists are part of the sampled classes
-        test_indices = [index for index, element in enumerate(test_dict['concat_labels']) if element in sampled_classes]
-        enroll_indices = [index for index, element in enumerate(enroll_dict['concat_labels']) if element in sampled_classes]
-        
-        print(f"There are {len(test_indices)} test samples that belong to the sampled classes")
-        print(f"There are {len(enroll_indices)} enroll samples that belong to the sampled classes")
-        
-        """
-        We first construct the test/enroll, as it is easier and we always use all of it, one file at a time
-        We will need a tensor with the embeddings of the files that are part of the sampled_classes: [n,192]
-        We will also need a ref_labels vector [n], where each element represents a class from sampled_classes
-        """
-        print("Creating test embeddings vector and the reference labels")
-        test_embs = test_dict['concat_features'][test_indices] #np.asarray([embs for index,embs in enumerate(test_dict['concat_features']) if index in test_indices])
-        test_ids = [test_dict['concat_slices'][index] for index in test_indices]
-        ref_labels = [test_dict['concat_labels'][index] for index in test_indices] #[label for index,label in enumerate(test_dict['concat_labels']) if index in test_indices]
-        
 
-        """
-        We sample the embeddings from k_shot audios in each sampled class
-        If audios are normal, it wil sample exactly k_shot audios per class
-        If audios are split, it will sample a variable number of window_audios, but still coming from k_shot audios
-        We don't oversample embs in a class to equalize the classes lengths at the moment, as we calculate the mean anyway
-        """
-        print("Creating enroll embeddings vector")
-        enroll_embs = []
-        enroll_labels = []
-        max_class_ids = 0
-        for label in sampled_classes:
-            
-            # We get all the uniq audio filenames in the sampled support class (label)
-            # In case the audios are split, that's not a problem, because it will get the windows too.
-            k_shot = cfg.k_shot
-            uniq_ids_enroll_class = sorted(set([enroll_dict['concat_slices'][index] for index in enroll_indices if enroll_dict["concat_labels"][index] == label]))        
-            
-            # In case we do not have enough audio files per class, we lower k_shot for this class at the moment            
-            if k_shot > len(uniq_ids_enroll_class):
-                k_shot = len(uniq_ids_enroll_class)
-            
-            # We extract the class embs as the embeddings coming from the sampled audios
-            sampled_enroll_class_ids = sorted(random.sample(uniq_ids_enroll_class,k_shot))
-            
-            class_embs = np.asarray([enroll_dict['concat_features'][index] for index, enroll_id in enumerate(enroll_dict['concat_slices']) if ((enroll_id in sampled_enroll_class_ids) and (enroll_dict['concat_labels'][index]==label))])
-            
-            # We don't use it yet, as we do not oversample in the case of using windows yet
-            if len(class_embs) > max_class_ids:
-                max_class_ids = len(class_embs)
-            
-            enroll_embs.extend(class_embs)
-            enroll_labels.extend([label]*len(class_embs))
+        test_embs,test_labels = sampler_query(test_dict, sampled_classes)
+        print(test_embs.shape)
+        enroll_embs, enroll_labels = sampler_support(enroll_dict,sampled_classes,k_shot=cfg.k_shot)
+        print(enroll_embs.shape)
+             
+        sampling_support_time = time.time()
+        sampling_support_duration = sampling_support_time - task_start_time
+        print(f"Time taken to extract enroll samples is {sampling_support_duration} s")
         
-        enroll_embs = np.asarray(enroll_embs)
-        enroll_labels = np.asarray(enroll_labels)
-
         # Choose to normalize embeddings or not
-
         if cfg.normalize == True:
             #enroll_embs = enroll_embs / (np.linalg.norm(enroll_embs, ord=2, axis=-1, keepdims=True))
             enroll_embs = embedding_normalize(enroll_embs)
             #test_embs = test_embs / (np.linalg.norm(test_embs, ord=2, axis=-1, keepdims=True))
             test_embs = embedding_normalize(test_embs)
 
+        pred_labels = simpleshot(enroll_embs, enroll_labels, test_embs, sampled_classes, method=cfg.method)
 
-        print("Calculating the mean class embeddings")
-        # Calculate the mean embeddings for each class in the support
-        avg_enroll_embs = []
-
-        for label in sampled_classes:
-            
-            indices = np.where(enroll_labels == label)
-            if cfg.method == "normal":
-                embedding = (enroll_embs[indices].sum(axis=0).squeeze()) / len(indices)
-            if cfg.method == "median":
-                embedding = np.median(enroll_embs[indices], axis=0)
-            
-            
-
-            avg_enroll_embs.append(embedding)
+        acc = compute_acc(test_labels,pred_labels,sampled_classes)
         
-        avg_enroll_embs = np.asarray(avg_enroll_embs)
-
-        print(avg_enroll_embs.shape)
-        
-        # Calculate cosine similarity between test embeddings and the transpose of the averaged class embeddings
-        scores = np.matmul(test_embs, avg_enroll_embs.T)
-        matched_labels = scores.argmax(axis=-1)
-
-        """
-        Calculate score per audio, it's only one audio per file id in case of normal audios
-        In case of split audios (windows) there are multiple audios per file id, so majority voting must be performed
-        """
-
-        """
-        uniq_test_ids = sorted(list(set(test_ids))) # We go through each audio and get the majority class for it
-        
-        test_results = {}
-        for uniq_id in uniq_test_ids:
-            test_results[uniq_id] = {}
-            ids_indices = [index for index,id1 in enumerate(test_ids) if id1==uniq_id]
-
-            test_results[uniq_id]["ref"] = ref_labels[ids_indices[0]]
-            test_results[uniq_id]["pred"] = sampled_classes[majority_element(matched_labels[ids_indices])]
-
-        total_preds = 0
-        correct_preds = 0
-        for key in test_results.keys():
-            total_preds += 1
-            if test_results[key]["ref"] == test_results[key]["pred"]:
-                correct_preds += 1
-        """
-        total_preds = 0
-        correct_preds = 0
-
-        class_acc = {}
-        for cls in sampled_classes:
-            class_acc[cls] = {}
-            class_acc[cls]['total_preds'] = 0
-            class_acc[cls]['correct_preds'] = 0
-            class_acc[cls]['preds'] = []
-
-        # label in matched_labels is the position of a class in sampled_classes, from argmax
-        # matched_labels and ref_labels have the same size.
-        for (j,label) in enumerate(matched_labels):
-            total_preds += 1
-            class_acc[ref_labels[j]]['total_preds'] +=1
-
-            pred_class = sampled_classes[label]
-            class_acc[ref_labels[j]]['preds'].append(pred_class)
-
-            if pred_class == ref_labels[j]:
-                correct_preds += 1
-                class_acc[ref_labels[j]]['correct_preds'] +=1
-
-        acc = 100*(correct_preds/total_preds)
         task_accs.append(acc)
         logger.info(f"Accuracy for task {i} is {acc}%.")
+        computing_duration = time.time() - task_start_time
+        print(f"Time taken by task {i} is {computing_duration} s")
 
     final_acc = sum(task_accs)/len(task_accs)
     logger.info(f"Final accuracy over {cfg.n_tasks} tasks is {final_acc}%.")
---
+++ cvn_sr/utils/utils.py
@@ -0,0 +1,101 @@
+import os
+import numpy as np
+
+def sampler_query(test_dict,sampled_classes):
+    # We find which indices in the lists are part of the sampled classes
+    test_indices = [index for index, element in enumerate(test_dict['concat_labels']) if element in sampled_classes]
+            
+    """
+    We first construct the test/query, as it is easier and we always use all of it, one file at a time
+    We will create a tensor of size [n_query,192], where n_query are all queries that are part of the sampled classes.
+    """
+    print("Creating test embeddings vector and the reference labels")
+    test_embs = test_dict['concat_features'][test_indices] 
+    test_labels = np.asarray([test_dict['concat_labels'][index] for index in test_indices]) 
+
+    return test_embs, test_labels
+
+def sampler_support(enroll_dict, sampled_classes,k_shot):
+    enroll_indices = [index for index, element in enumerate(enroll_dict['concat_labels']) if element in sampled_classes]
+    """
+    We sample the embeddings from k_shot audios in each sampled class
+    If audios are normal, it wil sample exactly k_shot audios per class
+    If audios are split, it will sample a variable number of window_audios, but still coming from k_shot audios
+    We don't oversample embs in a class to equalize the classes lengths at the moment, as we calculate the mean anyway
+    """
+    
+    all_enroll_labels = np.array(enroll_dict['concat_labels'])
+    value_masks = [all_enroll_labels == class_name for class_name in sorted(sampled_classes)]
+    # Extract indices for each value
+    value_indices = [np.where(mask)[0] for mask in value_masks]
+
+    # We do this in order to not take more samples from a class than the existing maximum, or simply not duplicate samples in classes when extracting
+    
+    if k_shot > 10:
+        k_shot_list = []
+        for indices in value_indices:
+            if k_shot > len(indices):
+                k_shot_list.append(len(indices))
+            else:
+                k_shot_list.append(k_shot)
+
+        # Shuffle the indices for each value
+        enroll_indices = np.concatenate([np.random.choice(indices, size=k_shot_list[index], replace=False) for index,indices in enumerate(value_indices)])
+    else:
+        enroll_indices = np.concatenate([np.random.choice(indices, size=k_shot, replace=False) for indices in value_indices])
+    
+    enroll_embs = enroll_dict['concat_features'][enroll_indices]
+    enroll_labels = all_enroll_labels[enroll_indices]
+
+    return enroll_embs, enroll_labels
+
+def simpleshot(enroll_embs,enroll_labels,test_embs,sampled_classes,method="mean"):
+
+    print("Using SimpleShot method")
+    # Calculate the mean embeddings for each class in the support
+    avg_enroll_embs = []
+
+    for label in sampled_classes:
+        
+        indices = np.where(enroll_labels == label)
+        if method == "normal":
+            embedding = (enroll_embs[indices].sum(axis=0).squeeze()) / len(indices)
+        if method == "median":
+            embedding = np.median(enroll_embs[indices], axis=0)
+
+        avg_enroll_embs.append(embedding)
+    
+    avg_enroll_embs = np.asarray(avg_enroll_embs)
+    
+    # Calculate cosine similarity between test embeddings and the transpose of the averaged class embeddings
+    scores = np.matmul(test_embs, avg_enroll_embs.T)
+    matched_labels = scores.argmax(axis=-1)
+    pred_labels = [sampled_classes[label] for label in matched_labels]
+
+    return pred_labels
+
+def compute_acc(pred_labels,test_labels,sampled_classes):
+    total_preds = 0
+    correct_preds = 0
+
+    class_acc = {}
+    for cls in sampled_classes:
+        class_acc[cls] = {}
+        class_acc[cls]['total_preds'] = 0
+        class_acc[cls]['correct_preds'] = 0
+        class_acc[cls]['preds'] = []
+
+    # label in matched_labels is the position of a class in sampled_classes, from argmax
+    # matched_labels and test_labels have the same size.
+    for (j,label) in enumerate(pred_labels):
+        total_preds += 1
+        class_acc[test_labels[j]]['total_preds'] +=1
+        class_acc[test_labels[j]]['preds'].append(label)
+
+        if label == test_labels[j]:
+            correct_preds += 1
+            class_acc[test_labels[j]]['correct_preds'] +=1
+
+    acc = 100*(correct_preds/total_preds)
+
+    return acc
